f6350ce308b41938d2ad2b84140533e02c26455a
Argjend <argjend.nimanaj@student.uni-pr.edu>
2024-04-20 18:29:31 +0200

    Full Commit

---
+++ admin/config/constants.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+define('ROOT_URL', 'http://localhost/Blogging Website/blog/');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'Argjend');
+define('DB_PASS', 'admin1234');
+define('DB_NAME', 'blog');
